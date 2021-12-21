76b47e3c0fa132d16bd4b53ea4042797b6e3d46f
David Whitney <david@davidwhitney.co.uk>
2021-12-21 11:49:34 +0000

    day2-2

--- src/day2.test.ts
+++ src/day2.test.ts
@@ -22,10 +22,11 @@ describe("Submarine", () => {
     it("Test", () => {
         const contents = readFileSync("./src/day2.txt").toString().split("\n");
 
-        const sut = new Submarine();
-        sut.process(contents);
+        const sut = new Submarine().process(contents);
+        const sut2 = new Submarine(true).process(contents);
 
         expect(sut.output).toBe(1690020);
+        expect(sut2.output).toBe(1408487760);
     });
 
 });
@@ -34,28 +35,61 @@ class Submarine {
 
     public horizontal: number;
     public depth: number;
+    public aim: number;
+
+    private _useAim: boolean;
+
     public get output() {
         return this.horizontal * this.depth;
     };
 
-    constructor() {
+    constructor(useAim: boolean = false) {
+        this._useAim = useAim;
         this.horizontal = 0;
         this.depth = 0;
+        this.aim = 0;
     }
 
     public process(instructions: string[]) {
         for (const instruction of instructions) {
             this.processSingle(instruction);
         }
+        return this;
     }
 
     private processSingle(instruction: string) {
         const [direction, value] = instruction.split(" ");
+        const valueNumber = parseInt(value);
 
         switch (direction) {
-            case "forward": this.horizontal += parseInt(value); break;
-            case "down": this.depth += parseInt(value); break;
-            case "up": this.depth -= parseInt(value); break;
+            case "forward": {
+                this.horizontal += valueNumber;
+
+                if (this._useAim) {
+                    this.depth += (this.aim * valueNumber);
+                }
+
+                break;
+            }
+
+            case "down": {
+                if (this._useAim) {
+                    this.aim += valueNumber;
+                } else {
+                    this.depth += valueNumber;
+                }
+                break;
+            }
+
+            case "up": {
+                if (this._useAim) {
+                    this.aim -= valueNumber;
+                } else {
+                    this.depth -= valueNumber;
+                }
+                break;
+            }
+
             default: break;
         }
 
